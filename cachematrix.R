6fc3ba0c59b0d36aaeab2e6bc2936a7849824b40
nicjongray <nicholas.jon.gray@gmail.com>
2021-07-30 18:06:14 +1000

    Update R script with required functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## BY Nicholas, <<EMAIL@gmail.com>>
+## Can be used to take inverse of Matrices, but with added check to see if inverse
+## already taken. Will help with large matrices so inverse doesn't need to be 
+## calculated more than once.
+## e.g.
+## > mtx <- makeCacheMatrix(matrix(rnorm(9),3,3))
+## > inverse_mtx <- cacheSolve(mtx)
 
-## Write a short comment describing this function
 
+## Function to take create "special matrix"
+## Input: Numerical Matrix e.g. matrix(rnorm(9),3,3)
+## Output: List of functions
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) m <<- solve
+        getinv <- function() m
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 }
 
 
-## Write a short comment describing this function
-
+## Function to take inverse of Matrix, but checks to see if 
+## inverse already exists in Cache
+## Input: "Special" Matrix output from `makeCacheMatrix`
+## Output: Inverse of matrix
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinv()
+        if(!is.null(m)) {
+              message("getting cached data")
+              return(m)
+        }
+        data <- x$get()
+        m <- solve(data)
+        x$setinv(m)
+        m
 }
